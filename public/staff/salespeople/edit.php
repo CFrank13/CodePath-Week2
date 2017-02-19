940673bbb5913d8c12490b24b5c764d59131a782
GinaRatto <ginagratto@gmail.com>
2017-02-18 16:40:38 -0800

    Objective 4 stuff, untested though

--- public/staff/salespeople/edit.php
+++ public/staff/salespeople/edit.php
@@ -8,6 +8,21 @@ $salespeople_result = find_salesperson_by_id($_GET['id']);
 // No loop, only one result
 $salesperson = db_fetch_assoc($salespeople_result);
 
+if(is_post_request()) {
+  // Confirm that values are present before accessing them.
+  if(isset($_POST['first_name'])) { $salesperson['first_name'] = h($_POST['first_name']); }
+  if(isset($_POST['last_name'])) { $salesperson['last_name'] = h($_POST['last_name']); }
+  if(isset($_POST['phone'])) { $salesperson['phone'] = h($_POST['phone']); }
+  if(isset($_POST['email'])) { $salesperson['email'] = h($_POST['email']); }
+  $result = update_salesperson($salesperson);
+  if($result === true) {
+    redirect_to('show.php?id=' . $salesperson['id']);
+  } else {
+    $errors = $result;
+  }
+}
+
+
 ?>
 <?php $page_title = 'Staff: Edit Salesperson ' . $salesperson['first_name'] . " " . $salesperson['last_name']; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
@@ -17,7 +32,20 @@ $salesperson = db_fetch_assoc($salespeople_result);
 
   <h1>Edit Salesperson: <?php echo $salesperson['first_name'] . " " . $salesperson['last_name']; ?></h1>
 
-  <!-- TODO add form -->
+  <?php echo display_errors($errors); ?>
+
+  <form action="edit.php?id=<?php echo $salesperson['id']; ?>" method="post">
+    First name:<br />
+    <input type="text" name="first_name" value="<?php echo $salesperson['first_name']; ?>" /><br />
+    Last name:<br />
+    <input type="text" name="last_name" value="<?php echo $salesperson['last_name']; ?>" /><br />
+    Phone:<br />
+    <input type="text" name="phone" value="<?php echo $salesperson['phone']; ?>" /><br />
+    Email:<br />
+    <input type="text" name="email" value="<?php echo $salesperson['email']; ?>" /><br />
+    <br />
+    <input type="submit" name="submit" value="Update"  />
+  </form>
 
 </div>
 
